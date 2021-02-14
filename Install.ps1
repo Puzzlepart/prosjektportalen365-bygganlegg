1e4071eaf779a540b0576a91e3b7c5ae63450750
Tarjei E. Ormestøyl <ormestoyl@gmail.com>
2021-02-14 10:59:41 +0100

    Include portfolio in install

--- Install.ps1
+++ Install.ps1
@@ -85,7 +85,7 @@ if ($CurrentContext.Url -ne $Url) {
 $BasePath =  "$PSScriptRoot\Templates"
 
 Write-Host "[INFO] Applying PnP template [Portfolio] to [$Url]"
-#Apply-PnPProvisioningTemplate "$BasePath\Portfolio\Portfolio.xml" -ErrorAction Stop
+Apply-PnPProvisioningTemplate "$BasePath\Portfolio\Portfolio.xml" -ErrorAction Stop -Handlers Lists
 Write-Host "[SUCCESS] Successfully applied PnP template [Portfolio] to [$Url]" -ForegroundColor Green
 
 if ($IncludeBygg.IsPresent) {
